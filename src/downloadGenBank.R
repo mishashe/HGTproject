6d7618f5324b637d5ba9cb40d1e033f58d2f6c64
Florian Massip <florian.massip@mines-paristech.fr>
2021-12-15 09:49:21 +0100

    add option for species name

--- src/downloadGenBank.R
+++ src/downloadGenBank.R
@@ -4,12 +4,26 @@ library(phangorn)
 library(R.utils)
 library(stringr)
 restez_disconnect()
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) < 1 | args[1] == '--help'){
+  message("usage: downloadGenBank.R species")
+  stop()
+}
+
+species=args[1]
+
+
 # import data from JGI
-Table <- read.table(file="~/HGTnew/data/external/EscherichiaColi.csv",sep="\t", row.names=NULL,header=TRUE)
+Table <- read.table(paste(file="~/HGTnew/data/external/",species,".csv",sep='')
+                    ,sep="\t", row.names=NULL,header=TRUE)
 restez_path_set("~/HGTnew/data/external")
 restez_connect()
 
-# get Escherichia coli from all countries
+species_dir=paste("~/HGTnew/data/processed/",species,sep="")
+comm=paste("mkdir -p", species_dir)
+system(comm)
+# get species from all countries
 countries <- unique(Table$Isolation.Country)
 countries <- str_replace(countries," ","");countries <- str_replace(countries," ","");countries <- str_replace(countries," ","")
 countries <- countries[countries!=""]
@@ -19,14 +33,14 @@ for (country in countries)
   ACCN <- Table$NCBI.Biosample.Accession[Table$Isolation.Country==country & Table$High.Quality=="Yes"]
   ACCN <- ACCN[ACCN!=""]
   i <- 1
-  write(data.frame(), paste0("~/HGTnew/data/processed/EscherichiaColi/EscherichiaColi_",country,".fasta"),append=FALSE)
+  write(data.frame(), paste0(species_dir,"/",species,"_",country,".fasta"),append=FALSE)
   while (i<=length(ACCN))
   {
     term <- paste(ACCN[i:min(i+4,length(ACCN))],collapse="[ACCN] OR ")
     IDs <- rentrez::entrez_search(db   = "biosample",term = term,retmax=101, use_history=FALSE)$ids
     linked_seq_ids <- rentrez::entrez_link(dbfrom="biosample", id=IDs, db="nuccore",retmax=40000,term="(100000[SLEN] : 1000000000000000[SLEN])", use_history=FALSE)
     seq <- rentrez::entrez_fetch(db="nuccore", id=linked_seq_ids$links$biosample_nuccore, rettype="fasta",retmax=101)
-    write(seq, paste0("~/HGTnew/data/processed/EscherichiaColi/EscherichiaColi_",country,".fasta"),append=TRUE)
+    write(seq, paste0(species_dir,"/",species,"_",country,".fasta"),append=TRUE)
     print(paste0(i," from ",length(ACCN)))
     i <- i+5
   }
