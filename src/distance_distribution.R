dd79900dbd5ee3337d64c28f2ced92d46dcfc4b5
Florian Massip <florian.massip@gmail.com>
2022-05-23 20:05:10 +0200

    change xlab and bin size

--- src/distance_distribution.R
+++ src/distance_distribution.R
@@ -23,7 +23,7 @@ hh=rep(0,200)
 cc=0
 for (i in files1){
     a=read.table(i)
-    h1=hist(a$V3,breaks = seq(0,1,0.005),plot=F)
+    h1=hist(a$V3,breaks = seq(0,1,0.001),plot=F)
     hh=hh+h1$counts
     cc=cc+1
     print(i)
@@ -34,7 +34,7 @@ for (i in files1){
     if (  cc / 50 == round(cc/50)){
       h1$counts=hh
       pdf(paste("~/HGTnew/plots/pairwise_distances/",species1,".pdf",sep=''))
-      plot(h1,xlim=c(0,0.35),ylim=c(0,max(h1$counts[1:70])),
+      plot(h1,xlim=c(0,0.15),ylim=c(0,max(h1$counts[1:70])),
            main = paste(cc,"files -- ",species1),
            xlab="distance")
       dev.off()
@@ -43,7 +43,7 @@ for (i in files1){
   }
       h1$counts=hh
       pdf(paste("~/HGTnew/plots/pairwise_distances/",species1,".pdf",sep=''))
-      plot(h1,xlim=c(0,0.35),ylim=c(0,max(h1$counts[1:70])),
+      plot(h1,xlim=c(0,0.15),ylim=c(0,max(h1$counts[1:70])),
            main = paste(cc,"files -- ",species1),
            xlab="distance")
       dev.off()
