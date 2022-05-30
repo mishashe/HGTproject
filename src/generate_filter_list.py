7a553f07bf24029e5fdd32e18c55ffe05e4d0211
Florian Massip <florian.massip@gmail.com>
2022-05-30 16:16:04 +0200

    fix some bugs in Mash filtering / calculating A

--- src/generate_filter_list.py
+++ src/generate_filter_list.py
@@ -38,25 +38,24 @@ def AssembToRemove(species,country,threshold=threshold):
 	filterFile="/cluster/CBIO/data1/fmassip/HGT/ProjectMisha/HGTnew/data/processed/toFilterMash/"+species+"/"+species+"-"+country+"/toFilter.txt"
 
 	AssembToRemove=[]
-	if os.path.exists(filterFile):
-		op=open(filterFile,"r")
-		for line in op.readlines():
-			line=line.rstrip()
-			AssembToRemove.append(line)
-	else:
-		comm="mkdir -p /cluster/CBIO/data1/fmassip/HGT/ProjectMisha/HGTnew/data/processed/toFilterMash/"+species+"/"+species+"-"+country
-		os.system(comm)
-		fileRegex = re.compile(species+"-"+country)
-		testFileSp = lambda x: fileRegex.search(x)
-		myFiles = filter(testFileSp,
-		os.listdir(myDir+species))
+	comm="mkdir -p /cluster/CBIO/data1/fmassip/HGT/ProjectMisha/HGTnew/data/processed/toFilterMash/"+species+"/"+species+"-"+country
+	os.system(comm)
+	fileRegex = re.compile(species+"-"+country)
+	testFileSp = lambda x: fileRegex.search(x)
+	myFiles = filter(testFileSp,
+	os.listdir(myDir+species))
 
 
-		for i in myFiles:
-			print(i)
+	for i in myFiles:
+		print(i)
+		print(species+"-"+country+"-"+country)
+		if i == species+"-"+country+"-"+country :
+			print("do not use this file to filter")
+		else:
 			myPath=myDir+species+"/"+i+"/dist-close.txt"
 			if os.path.exists(myPath):
 				op = open(myPath, 'r')
+				thisCount = 0
 				for line in op.readlines():
 					line = line.rstrip()
 					row = line.split('\t')
@@ -64,17 +63,18 @@ def AssembToRemove(species,country,threshold=threshold):
 					thisAssemb2 = row[1]
 					dist = float(row[2])
 
-					if dist <= threshold : 
-
+					if dist < threshold : 
+						thisCount = thisCount + 1
 						thisAssemb1=re.sub(r".+\/(.*).fasta",r"\1",thisAssemb1)
 						thisAssemb2=re.sub(r".+\/(.*).fasta",r"\1",thisAssemb2)
 						AssembToRemove.append(thisAssemb1)
-						AssembToRemove.append(thisAssemb2)
+	#					AssembToRemove.append(thisAssemb2)
+				print("count: "+str(thisCount))
 		
-		AssembToRemove=set(AssembToRemove)
-		op=open(filterFile,"w")
-		op.write("\n".join(i for i in AssembToRemove)+"\n")
-		op.close()
+	AssembToRemove=set(AssembToRemove)
+	op=open(filterFile,"w")
+	op.write("\n".join(i for i in AssembToRemove)+"\n")
+	op.close()
 
 	return(AssembToRemove)
 
